95047e6ef3cce8ff55fc6f51548b79b88b6804eb
Artem 'Aristarhys' Bashev <artem.bashev@gmail.com>
2015-09-22 15:55:32 +0300

    Changed index page to describe active VMachines count and project summary

--- src/vmango/handlers/index.go
+++ src/vmango/handlers/index.go
@@ -3,11 +3,18 @@ package handlers
 import (
 	"net/http"
 	"vmango"
+	"vmango/models"
 )
 
 func Index(ctx *vmango.Context, w http.ResponseWriter, req *http.Request) error {
+	machines := []*models.VirtualMachine{}
+	err, activeMachinesCount := ctx.Machines.List(&machines);
+	if err != nil {
+		return err
+	}
 	ctx.Render.HTML(w, http.StatusOK, "index", map[string]interface{}{
 		"Request": req,
+		"ActiveMachinesCount": activeMachinesCount,
 	})
 	return nil
 }
